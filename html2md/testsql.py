99b2068a9851237ca43af58b80d894689cc031ac
ugoMusk <ugobothankgod@gmail.com>
2023-10-20 08:16:31 +0000

    attempt connection with sqlalchemy, fingers crossed

--- html2md/testsql.py
+++ html2md/testsql.py
@@ -4,11 +4,11 @@ import subprocess
 
 
 ip = subprocess.check_output(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "html2mddb"]).decode("utf-8").strip()
-print(f"ip: {ip}")
+print(f"database ip address: {ip}")
 # Create an engine to connect to the MySQL database using the user credentials and the host name of the MySQL container
-#engine = sqlalchemy.create_engine("mysql+mysqldb://$MYSQL_USER:MYSQL_PASSWORD@my{}/html2md".format(ip))
+engine = sqlalchemy.create_engine("mysql+mysqldb://$MYSQL_USER:MYSQL_PASSWORD@my{}/html2md".format(ip))
 
 # Run some queries on the database using a connection object
-#with engine.connect() as conn:
-    # result = conn.execute("SHOW TABLES;")
-    # print(result.fetchall())
+with engine.connect() as conn:
+    result = conn.execute("SHOW TABLES;")
+    print(result.fetchall())

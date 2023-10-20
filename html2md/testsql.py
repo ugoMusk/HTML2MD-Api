49bcc171c17c75e5d689e1c80346b157a5815388
ugoMusk <ugobothankgod@gmail.com>
2023-10-20 09:06:39 +0000

    attempt connection with sqlalchemy, get and use values from  environment variables

--- html2md/testsql.py
+++ html2md/testsql.py
@@ -1,12 +1,16 @@
 # Import the sqlalchemy module
+import os
 import sqlalchemy
 import subprocess
 
 
+mysqlRootUser = os.environ.get("MYSQL_ROOT_USER")
+mysqlRootPassword = os.environ.get("MYSQL_ROOT_PASSWORD")
+
 ip = subprocess.check_output(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "html2mddb"]).decode("utf-8").strip()
 print(f"database ip address: {ip}")
 # Create an engine to connect to the MySQL database using the user credentials and the host name of the MySQL container
-engine = sqlalchemy.create_engine("mysql+mysqldb://$MYSQL_USER:MYSQL_PASSWORD@{}/html2md".format(ip))
+engine = sqlalchemy.create_engine("mysql+mysqldb://{}:{}@{}/html2md".format(mysqlRootUser, mysqlRootPassword, ip))
 
 # Run some queries on the database using a connection object
 with engine.connect() as conn:

a9196de48d9adca428116c14bf7599923afcbf7b
ugoMusk <ugobothankgod@gmail.com>
2023-10-20 07:16:48 +0000

    try connecting to database using python

--- html2md/testsql.py
+++ html2md/testsql.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 ip = subprocess.check_output(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "html2mddb"]).decode("utf-8").strip()
-print(ip)
+print("ip:" ip)
 # Create an engine to connect to the MySQL database using the user credentials and the host name of the MySQL container
 #engine = sqlalchemy.create_engine("mysql+mysqldb://$MYSQL_USER:MYSQL_PASSWORD@my{}/html2md".format(ip))
 

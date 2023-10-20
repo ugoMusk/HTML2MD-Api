64aa42d41182ddf6b4807fc43addea80f0d1f6d3
ugoMusk <ugobothankgod@gmail.com>
2023-10-20 05:01:21 +0000

    try connecting to database using python

---
+++ html2md/testsql.py
@@ -0,0 +1,10 @@
+# Import the sqlalchemy module
+import sqlalchemy
+
+# Create an engine to connect to the MySQL database using the user credentials and the host name of the MySQL container
+engine = sqlalchemy.create_engine("mysql+psycopg2://"$MYSQL_USER":"MYSQL_PASSWORD@mysql-html2md/html2md")
+
+# Run some queries on the database using a connection object
+with engine.connect() as conn:
+    result = conn.execute("SHOW TABLES;")
+    print(result.fetchall())

ef1b87df76f999c1bf972434a5ef1cd8187245ca
Sue609 <susankamu609@gmail.com>
2023-10-24 09:33:45 +0300

    implement github rest api

--- html2md/api/engine/api.py
+++ html2md/api/engine/api.py
@@ -2,60 +2,44 @@ import base64
 import requests
 
 
-def get_current_file_sha(repo_owner, repo_name, file_path_in_repo, github_token):
-    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path_in_repo}"
-    response = requests.get(api_url, headers={"Authorization": f"Bearer {github_token}"})
-
-
-    if response.status_code == 200:
-        file_info = response.json()
-        return file_info.get("sha")
-    else:
-        print(f"Failed to fetch file info from GitHub. Status code: {response.status_code}")
-        return None
+def get_user_input():
+    """
+    Reads and accepts users input
+    """
+    file_path = input("Enter the path to the file you want to upload: ")
+    repo_owner = input("Enter the GitHub repository owner: ")
+    repo_name = input("Enter the GitHub repository name: ")
+    file_path_in_repo = input("Enter the desired file name in the repository (including extension): ")
+    github_token = input("Enter your GitHub token: ")
+    return file_path, repo_owner, repo_name, file_path_in_repo, github_token
 
 
 def upload_file_to_github(file_path, repo_owner, repo_name, file_path_in_repo, github_token):
+    """
+    Uploads file to github using github rest Api
+    """
     try:
-        # Read the file content and encode it to base64
         with open(file_path, 'rb') as file_content:
             base64_content = base64.b64encode(file_content.read()).decode()
 
-
-        # Get the current SHA of the file from GitHub
-        
         json_payload = {
             "message": "Upload file",
             "content": base64_content,
-            # "sha": current_sha,  # Include the current SHA in the payload
         }
 
-
-        # GitHub API endpoint
         api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path_in_repo}"
 
-
-        # Upload the file to GitHub using requests library
         response = requests.put(api_url, json=json_payload, headers={"Authorization": f"Bearer {github_token}"})
 
-
         if response.status_code == 201:
-                print("File uploaded to GitHub successfully.")
+            print("File uploaded to GitHub successfully.")
         else:
             print(f"Failed to upload file to GitHub. Status code: {response.status_code}")
-            print(response.content)  # Print response content for debugging
+            print(response.content)
 
     except Exception as e:
         print(f"Error: {e}")
 
+file_path, repo_owner, repo_name, file_path_in_repo, github_token = get_user_input()
 
-# Example usage
-file_path = "scarb.md"
-repo_owner = "Sue609"
-repo_name = "test"
-file_path_in_repo = "README.md"
-github_token = "GITHUB_TOKEN"
-
-
-# Call the function to upload the file to GitHub
-# upload_file_to_github(file_path, repo_owner, repo_name, file_path_in_repo, github_token)
+upload_file_to_github(file_path, repo_owner, repo_name, file_path_in_repo, github_token)
--- html2md/api/engine/git-api.py
+++ html2md/api/engine/git-api.py
@@ -2,10 +2,24 @@ import base64
 import requests
 
 
+def get_user_input():
+    """
+    Reads and accepts users input
+    """
+    file_path = input("Enter the path to the file you want to upload: ")
+    repo_owner = input("Enter the GitHub repository owner: ")
+    repo_name = input("Enter the GitHub repository name: ")
+    file_path_in_repo = input("Enter the desired file name in the repository (including extension): ")
+    github_token = input("Enter your GitHub token: ")
+    return file_path, repo_owner, repo_name, file_path_in_repo, github_token
+
+
 def get_current_file_sha(repo_owner, repo_name, file_path_in_repo, github_token):
+    """
+    Gets the current file sha from github.
+    """
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path_in_repo}"
     response = requests.get(api_url, headers={"Authorization": f"Bearer {github_token}"})
-
 
     if response.status_code == 200:
         file_info = response.json()
@@ -16,54 +30,37 @@ def get_current_file_sha(repo_owner, repo_name, file_path_in_repo, github_token)
 
 
 def upload_file_to_github(file_path, repo_owner, repo_name, file_path_in_repo, github_token):
+    """
+    Uploads file to github using the github api.
+    """
     try:
-        # Read the file content and encode it to base64
         with open(file_path, 'rb') as file_content:
             base64_content = base64.b64encode(file_content.read()).decode()
 
-
-        # Get the current SHA of the file from GitHub
         current_sha = get_current_file_sha(repo_owner, repo_name, file_path_in_repo, github_token)
-
 
         if current_sha:
             json_payload = {
                 "message": "Upload file",
                 "content": base64_content,
-               "sha": current_sha,  # Include the current SHA in the payload
+                "sha": current_sha,
             }
 
-
-            # GitHub API endpoint
             api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path_in_repo}"
 
-
-            # Upload the file to GitHub using requests library
             response = requests.put(api_url, json=json_payload, headers={"Authorization": f"Bearer {github_token}"})
-
 
             if response.status_code == 201:
                 print("File uploaded to GitHub successfully.")
             else:
                 print(f"Failed to upload file to GitHub. Status code: {response.status_code}")
-                print(response.content)  # Print response content for debugging
-
+                print(response.content)
 
         else:
             print("Failed to obtain current SHA of the file from GitHub.")
 
-
     except Exception as e:
         print(f"Error: {e}")
 
-
-# Example usage
-file_path = "scarb.md"
-repo_owner = "Sue609"
-repo_name = "test"
-file_path_in_repo = "README.md"
-github_token = "GITHUB_TOKEN"
-
-
-# Call the function to upload the file to GitHub
-# upload_file_to_github(file_path, repo_owner, repo_name, file_path_in_repo, github_token)
+file_path, repo_owner, repo_name, file_path_in_repo, github_token = get_user_input()
+upload_file_to_github(file_path, repo_owner, repo_name, file_path_in_repo, github_token)

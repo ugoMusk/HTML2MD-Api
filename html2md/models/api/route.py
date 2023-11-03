697ee8d5faeea3997ac3f8c790c823e77ede06dd
ugoMusk <ugobothankgod@gmail.com>
2023-11-03 10:33:12 +0100

    updates

--- html2md/models/api/route.py
+++ html2md/models/api/route.py
@@ -6,7 +6,7 @@ import os
 import sys
 modelPath = os.path.abspath("../../models")
 sys.path.append(modelPath)
-from flask import Flask, jsonify, send_file, render_template
+from flask import Flask, jsonify, send_file, render_template, url_for, redirect
 from engine.engine import downloadUrl, convertHtml2Markdown
 from storage import cookies
 from flask_cors import CORS
@@ -14,9 +14,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
-@app.route("/convert/url/<userUrl>", methods=['GET'], strict_slashes=False)
 
-#@app.route("/convert/url/<path:userUrl>", methods=['GET'], strict_slashes=False)
+@app.route("/convert/url/<path:userUrl>", methods=['GET'], strict_slashes=False)
 def getMarkdown(userUrl):
     """ 
     route method to parse html to markdown
@@ -24,7 +23,7 @@ def getMarkdown(userUrl):
     print("Received URL:", userUrl)
     res = downloadUrl(userUrl)
     
-    return render_template('render_temp.html', result=res)
+    return render_template('render_temp.html')
     #return res
 
 
@@ -33,13 +32,16 @@ def convertMarkdown():
     """ 
     route method to parse html to markdown
     """
-    with open("scrab.html", mode="r", encoding="utf-8") as htmlFile:
-        html = htmlFile.read()
+    try:
+        with open("scrab.html", mode="r", encoding="utf-8") as htmlFile:
+            html = htmlFile.read()
+    except FileNotFoundError:
+        html = "<body>sacrab.md not found</body>"
 
     res = convertHtml2Markdown(html)
     
-    # return render_template('render_temp.html', result=res)
-    return res
+    return render_template('render_temp.html', result=res)
+    # return res
 
 
 @app.route("/download", methods=['GET'], strict_slashes=False)
@@ -47,24 +49,34 @@ def downloadFile():
     """
     specify the path to the file to download
     """
-    filePath = '/home/samke/HTML2MD-Api/html2md/models/engine/scab.md'
-    return send_file(filePath, as_attachment=True)
-
+    filePath = 'scab.md'
+    try:
+        with open(filePath, mode="r", encoding="utf-8") as dFile:
+            mdFile = dFile.read()
+            sendFile = send_file(mdFile, as_attachment=True)
+    except FileNotFoundError:
+        sendFile = (f"{filePath} does not exists")
+    return sendFile
 @app.route("/")
-def setcookie_route():
+def setCookieRoute():
     # set a cookie with key "name" and value "Alice"
-    return cookies.setCookie()
+    return render_template("myr.html", cookies=cookies.setCookie())
 
 @app.route("/getcookie")
-def getcookie_route():
+def getCookieRoute():
     # get a cookie with key "name"
     return cookies.getCookie("name")
 
 @app.route("/deletecookie")
-def deletecookie_route():
+def deleteCookieRoute():
     # delete a cookie with key "name"
     return cookies.deleteCookie("name")
 
+@app.route("/redirect")
+def redirecRoute(url):
+    #Set a redirection path
+    url = url_for(url)
+    return redirect(url)
 
 if __name__ == "__main__":
     app.run()

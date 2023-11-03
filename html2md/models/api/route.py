ab93b3489cb3ff300f15cfd8bf99aaf100bbf19c
Sue609 <susankamu609@gmail.com>
2023-11-03 09:18:38 +0300

    implement frontend improving the clients output.

--- html2md/models/api/route.py
+++ html2md/models/api/route.py
@@ -6,25 +6,31 @@ import os
 import sys
 modelPath = os.path.abspath("../../models")
 sys.path.append(modelPath)
-from flask import Flask, jsonify, send_file, render_template
+from flask import Flask, jsonify, send_file, render_template, url_for 
 from engine.engine import downloadUrl, convertHtml2Markdown
 from storage import cookies
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 
 CORS(app)
-@app.route("/convert/url/<userUrl>", methods=['GET'], strict_slashes=False)
+
+@app.route("/convert/url/", methods=['GET'], strict_slashes=False)
+def handleMissingURL():
+    return "Error: No URL provided."
+
+@app.route("/convert/url/<path:userUrl>", methods=['GET'], strict_slashes=False)
 
 #@app.route("/convert/url/<path:userUrl>", methods=['GET'], strict_slashes=False)
 def getMarkdown(userUrl):
     """ 
     route method to parse html to markdown
     """
-    print("Received URL:", userUrl)
+    app.logger.info(f"Received URL: {userUrl}")
     res = downloadUrl(userUrl)
-    
-    return render_template('render_temp.html', result=res)
+    return send_file(res, as_attachment=True)
+    #return render_template('rapid.html', result=res)
     #return res
 
 
@@ -47,13 +53,14 @@ def downloadFile():
     """
     specify the path to the file to download
     """
-    filePath = '/home/samke/HTML2MD-Api/html2md/models/engine/scab.md'
+    filePath = '/mnt/c/Users/Susan/Desktop/html2md-api/HTML2MD-Api/html2md/models/api/scrab.md'
     return send_file(filePath, as_attachment=True)
 
 @app.route("/")
 def setcookie_route():
     # set a cookie with key "name" and value "Alice"
-    return cookies.setCookie()
+    #return cookies.setCookie()
+    return render_template("rapid.html")
 
 @app.route("/getcookie")
 def getcookie_route():
@@ -67,4 +74,4 @@ def deletecookie_route():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

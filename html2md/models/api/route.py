ca0853122713170ea2614df5b033af18d7bf3e7e
SAMKE-glitch <sammythemwa@gmail.com>
2023-11-08 11:35:30 +0300

    edit route file

--- html2md/models/api/route.py
+++ html2md/models/api/route.py
@@ -1,95 +1,77 @@
-<<<<<<< HEAD:html2md/models/api/route_v2.py
 #!/usr/bin/env python3
 """
 module defining our api endpoints
 """
 import os
+import io
 import sys
 modelPath = os.path.abspath("../../models")
 sys.path.append(modelPath)
-from flask import Flask, jsonify, send_file, render_template, url_for, redirect, request
+from flask import Flask, jsonify, send_file, render_template, url_for, request,redirect
 from engine.engine import downloadUrl, convertHtml2Markdown
 from storage import cookies
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 CORS(app)
+
 @app.route("/convert/url/", methods=['GET'], strict_slashes=False)
 def handleMissingURL():
     return "Error: No URL provided."
 
+@app.route("/convert/url/<path:userUrl>", methods=['GET'], strict_slashes=False)
 
-@app.route("/convert/url/<path:userUrl>", methods=['POST'], strict_slashes=False)
+#@app.route("/convert/url/<path:userUrl>", methods=['GET'], strict_slashes=False)
 def getMarkdown(userUrl):
     """ 
     route method to parse html to markdown
     """
-    userUrl = request.form.get('name')
-    res = downloadUrl(userUrl)
-    
-    return render_template('render_temp.html', result=res)
-    # return res
-    # convertedMd, html_file_path = downloadUrl(userUrl)
-    # if convertedMd:
-      #  return convertedMd
-    # else:
-      #  return "Error occurred during conversion", 500
+    convertedMd, html_file_path = downloadUrl(userUrl)
+    if convertedMd:
+        return convertedMd
+    else:
+        return "Error occurred during conversion", 500
 
-@app.route("/convert", methods=['GET'], strict_slashes=False)
-def convertMarkdown():
-    """ 
-    route method to parse html to markdown
-    """
+
+
+@app.route("/convert", methods=['POST'], strict_slashes=False)
+def convert():
     try:
-        with open("scrab.html", mode="r", encoding="utf-8") as htmlFile:
-            html = htmlFile.read()
-    except FileNotFoundError:
-        html = "<body>sacrab.md not found</body>"
-
-    res = convertHtml2Markdown(html)
+        data = request.get_json()
+        userHtml = data.get('userHtml', '')
+        convertedMD = convertHtml2Markdown(userHtml)
+        return convertedMD
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return "Error occurred during conversion"
     
-    return render_template('render_temp.html', result=res)
-    # return res
-
 
 @app.route("/download", methods=['GET'], strict_slashes=False)
 def downloadFile():
     """
     specify the path to the file to download
     """
-    filePath = 'scab.md'
-    try:
-        with open(filePath, mode="r", encoding="utf-8") as dFile:
-            mdFile = dFile.read()
-            sendFile = send_file(mdFile, as_attachment=True)
-    except FileNotFoundError:
-        sendFile = (f"{filePath} does not exists")
-    return sendFile
+    filePath = '/mnt/c/Users/Susan/Desktop/html2md-api/HTML2MD-Api/html2md/models/api/scrab.md'
+    return send_file(filePath, as_attachment=True)
 
 @app.route("/")
-def setCookieRoute():
+def setcookie_route():
     # set a cookie with key "name" and value "Alice"
-    return render_template("render_temp.html", cookies=cookies.setCookie())
     #return cookies.setCookie()
-    # return render_template("rapid.html")
+    return render_template("rapid.html")
 
 @app.route("/getcookie")
-def getCookieRoute():
+def getcookie_route():
     # get a cookie with key "name"
     return cookies.getCookie("name")
 
 @app.route("/deletecookie")
-def deleteCookieRoute():
+def deletecookie_route():
     # delete a cookie with key "name"
     return cookies.deleteCookie("name")
 
-@app.route("/redirect")
-def redirecRoute(url):
-    #Set a redirection path
-    url = url_for(url)
-    return redirect(url)
 
 if __name__ == "__main__":
     app.run(debug=True)
